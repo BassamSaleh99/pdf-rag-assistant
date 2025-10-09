e7d7dafaf2a5999ee4e92b67cf591a22e4e78187
BassamSaleh99 <bassam.saleh.1002@gmail.com>
2025-10-09 23:11:59 +0300

    Inngest Client

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from pdf-rag-assistant!")
+import logging
+from fastapi import FastAPI
+import inngest
+import inngest.fast_api
+from inngest.experimental import ai
+from dotenv import load_dotenv
+import uuid
+import os
+import datetime
+
+load_dotenv()
+
+inngest_client = inngest.Inngest(
+    app_id="rag_app",
+    logger=logging.getLogger("uvicorn"),
+    is_production=False,
+    serializer=inngest.PydanticSerializer()
+)
 
 
-if __name__ == "__main__":
-    main()
+app = FastAPI()
+
+
+inngest.fast_api.serve(app, inngest_client, [])
