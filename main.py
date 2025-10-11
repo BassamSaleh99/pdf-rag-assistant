11b20c2cbae077d4ce5ca232cf4edd7009ba6a14
BassamSaleh99 <bassam.saleh.1002@gmail.com>
2025-10-11 22:09:18 +0300

    inngest dev server setup

--- main.py
+++ main.py
@@ -17,8 +17,14 @@ inngest_client = inngest.Inngest(
     serializer=inngest.PydanticSerializer()
 )
 
+@inngest_client.create_function(
+    fn_id="RAG: Ingest PDF",
+    trigger=inngest.TriggerEvent(event="rag/ingest_pdf")
+)
+async def rag_ingest_pdf(ctx: inngest.Context):
+    return {"Hello": "world"}
 
 app = FastAPI()
 
 
-inngest.fast_api.serve(app, inngest_client, [])
+inngest.fast_api.serve(app, inngest_client, [rag_ingest_pdf])
