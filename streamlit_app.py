b70dee8ecbf519977dda88968090dbd6db9fed7c
BassamSaleh99 <bassam.saleh.1002@gmail.com>
2025-10-13 14:43:41 +0300

    get_inngest_client, save_uploaded_pdf

---
+++ streamlit_app.py
@@ -0,0 +1,27 @@
+import asyncio
+from pathlib import Path
+import time
+
+import streamlit as st
+import inngest
+from dotenv import load_dotenv
+import os
+import requests
+
+load_dotenv()
+
+st.set_page_config(page_title="RAG Ingest PDF", page_icon="📄", layout="centered")
+
+
+@st.cache_resource
+def get_inngest_client() -> inngest.Inngest:
+    return inngest.Inngest(app_id="rag_app", is_production=False)
+
+
+def save_uploaded_pdf(file) -> Path:
+    uploads_dir = Path("uploads")
+    uploads_dir.mkdir(parents=True, exist_ok=True)
+    file_path = uploads_dir / file.name
+    file_bytes = file.getbuffer()
+    file_path.write_bytes(file_bytes)
+    return file_path
